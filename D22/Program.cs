419ee6758ee5bbc9cac13976125a48656767cfc9
Martin Jaros <jarosm@centrum.cz>
2020-12-22 09:11:35 +0100

    Day 22

--- D22/Program.cs
+++ D22/Program.cs
@@ -8,17 +8,118 @@ namespace D22
 {
     class Program
     {
-        private static void D22a()
+        private static bool CheckHistory(List<int> player, List<List<int>> history)
         {
-            List<string> lines = new List<string>();
-            using (StreamReader input = File.OpenText("d:\\programming\\Advent of Code\\data 2020\\D22\\input.txt"))
+            foreach (var combination in history)
             {
-                string line = "";
-                while ((line = input.ReadLine()) != null)
+                if (combination.SequenceEqual(player))
+                    return true;
+            }
+            return false;
+        }
+
+
+        private static int Game(List<int> player1, List<int> player2)
+        {
+            var p1History = new List<List<int>>();
+            var p2History = new List<List<int>>();
+
+            while (player1.Any() && player2.Any())
+            {
+                if (CheckHistory(player1, p1History) && CheckHistory(player1, p1History))
+                    return 1;
+
+                p1History.Add(player1.ToList());
+                p2History.Add(player2.ToList());
+
+                var player1Card = player1.First();
+                var player2Card = player2.First();
+
+                player2.Remove(player2Card);
+                player1.Remove(player1Card);
+
+                if (player1Card <= player1.Count && player2Card <= player2.Count)
                 {
-                    lines.Add(line);
+                    var winner = Game(player1.Take(player1Card).ToList(), player2.Take(player2Card).ToList());
+
+                    if (winner == 1)
+                    {
+                        player1.Add(player1Card);
+                        player1.Add(player2Card);
+
+                    }
+                    else
+                    {
+                        player2.Add(player2Card);
+                        player2.Add(player1Card);
+                    }
+                }
+                else if (player1Card > player2Card)
+                {
+                    player1.Add(player1Card);
+                    player1.Add(player2Card);
+                }
+                else
+                {
+                    player2.Add(player2Card);
+                    player2.Add(player1Card);
                 }
             }
+
+            return player1.Any() ? 1 : 2;
+        }
+
+
+        private static void D22a()
+        {
+            var text = File.ReadAllText("d:\\programming\\Advent of Code\\data 2020\\D22\\input.txt");
+            var split = text.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            var player1 = split[0].Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList();
+            var player2 = split[1].Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList();
+
+            while (player1.Any() && player2.Any())
+            {
+                var player1Card = player1.First();
+                var player2Card = player2.First();
+
+                if (player1Card > player2Card)
+                {
+                    player2.Remove(player2Card);
+                    player1.Remove(player1Card);
+
+                    player1.Add(player1Card);
+                    player1.Add(player2Card);
+                }
+                else
+                {
+                    player2.Remove(player2Card);
+                    player1.Remove(player1Card);
+
+                    player2.Add(player2Card);
+                    player2.Add(player1Card);
+                }
+            }
+
+            var winner = player1.Any() ? player1 : player2;
+            var i = winner.Count;
+
+            Console.WriteLine("Part 1: " + winner.Aggregate(0L, (a, b) => a + (b * i--)).ToString());
+        }
+
+
+        private static void D22b()
+        {
+            var text = File.ReadAllText("d:\\programming\\Advent of Code\\data 2020\\D22\\input.txt");
+            var split = text.Split(new string[] { "\r\n\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            var player1 = split[0].Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList();
+            var player2 = split[1].Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(int.Parse).ToList();
+
+            Game(player1, player2);
+
+            var winner = player1.Any() ? player1 : player2;
+            var i = winner.Count;
+
+            Console.WriteLine("Part 2: " + winner.Aggregate(0L, (a, b) => a + (b * i--)).ToString());
 
             Console.WriteLine("end");
             Console.ReadLine();
@@ -28,6 +129,8 @@ namespace D22
         static void Main(string[] args)
         {
             D22a();
+
+            D22b();
         }
     }
 }
